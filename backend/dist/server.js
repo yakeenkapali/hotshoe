a7b57076f5471585b1c76c5aadc8b6b9f616181f
Shivaji Chalise <respond2shivaji@gmail.com>
2022-12-30 14:35:35 +0545

    Initialized a basic nodejs/typescript hello world api

---
+++ backend/dist/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+const server = express();
+const PORT = 8080;
+server.use(bodyParser.json()); // tells the app to use json throughout the app
+// ROUTES
+server.get('/', (req, res) => {
+    res.send('Hello World');
+});
+server.listen(PORT, () => console.log(`Server running on http://127.0.0.1:${PORT}`));
+//# sourceMappingURL=server.js.map
